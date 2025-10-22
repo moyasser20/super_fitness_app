0643d88c41561b61cd4d2b3ef712a607fe0df24d
omareldery99 <omar.eldery@acutebusiness.com>
2025-10-22 09:22:40 +0300

    - Fixed missing lines that missing while merging register and onboarding -splashScreen features

--- lib/core/routes/on_generate_route.dart
+++ lib/core/routes/on_generate_route.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:super_fitness_app/core/routes/route_names.dart';
-import 'package:super_fitness_app/features/splash/splash_screen.dart';
-
+import '../../features/app_sections/presentation/view/dash_board_screen.dart';
 import '../../features/auth/presentation/register/views/register_screen.dart';
 import '../../features/auth/presentation/register/views/complete_registration_screen.dart';
+import '../../features/splash/splash_screen.dart';
 
 class Routes {
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRoutes.initial:
-      case AppRoutes.register:
         return MaterialPageRoute(
           builder: (context) => const SplashScreen(),
           settings: settings,
         );
-       
+      case AppRoutes.register:
+        return MaterialPageRoute(
+          builder: (context) => const RegisterScreen(),
+          settings: settings,
+        );
+      case AppRoutes.dashboard:
+        return MaterialPageRoute(
+          builder: (context) => const DashboardScreenApp(),
+        );
+      case AppRoutes.completeRegistration:
+        return MaterialPageRoute(
+          builder: (context) => const CompleteRegistrationScreen(),
+        );
       default:
         return MaterialPageRoute(builder: (_) => const Scaffold());
     }
