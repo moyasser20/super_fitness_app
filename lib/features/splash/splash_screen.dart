0a286e0a291e457b44af48b1cda1ee52ae278ba6
omareldery99 <omar.eldery@acutebusiness.com>
2025-10-23 23:44:24 +0300

    - Delete unused imports.

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,13 +1,11 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:super_fitness_app/core/contants/secure_storage.dart';
 import 'package:super_fitness_app/core/routes/route_names.dart';
 import 'package:super_fitness_app/core/theme/app_colors.dart';
 import 'package:super_fitness_app/features/app_sections/presentation/view/dash_board_screen.dart';
-import 'package:super_fitness_app/features/auth/presentation/register/views/register_screen.dart';
 import 'package:super_fitness_app/features/onboarding/onboaarding_screen.dart';
-
 import '../../core/contants/prefs.dart';
-import '../auth/presentation/login/presentation/view/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -23,21 +21,19 @@ class _SplashScreenState extends State<SplashScreen> {
     _navigateToNextScreen();
   }
 
-  // splash_screen.dart
   Future<void> _navigateToNextScreen() async {
     await Future.delayed(const Duration(seconds: 3));
 
-    // Debug: Check what values we're getting
     final String? token = await SecureStorage.getToken();
     final bool hasSeenOnboarding = Prefs.isOnboardingSeen();
 
-    print('=== DEBUG SPLASH SCREEN ===');
-    print('Token: $token');
-    print('Has seen onboarding: $hasSeenOnboarding');
-    print('===========================');
+    log('=== DEBUG SPLASH SCREEN ===');
+    log('Token: $token');
+    log('Has seen onboarding: $hasSeenOnboarding');
+    log('===========================');
 
     if (token != null && token.isNotEmpty) {
-      print('Navigating to DASHBOARD');
+      log('Navigating to DASHBOARD');
       Navigator.pushAndRemoveUntil(
         context,
         MaterialPageRoute(builder: (context) => const DashboardScreen()),
@@ -45,14 +41,14 @@ class _SplashScreenState extends State<SplashScreen> {
       );
     } else {
       if (hasSeenOnboarding) {
-        print('Navigating to REGISTER (onboarding seen)');
+        log('Navigating to REGISTER (onboarding seen)');
         Navigator.pushNamedAndRemoveUntil(
           context,
           AppRoutes.login,
           (Route<dynamic> route) => false,
         );
       } else {
-        print('Navigating to ONBOARDING (first time)');
+        log('Navigating to ONBOARDING (first time)');
         Navigator.pushAndRemoveUntil(
           context,
           MaterialPageRoute(builder: (context) => const OnboardingScreen()),
